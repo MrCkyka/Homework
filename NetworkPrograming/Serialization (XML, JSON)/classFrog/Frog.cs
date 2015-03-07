cb7b99a4dff2df7f754e0e8a047cef0513d18289
mrckyka <namepodlec@yandex.ru>
2015-03-07 10:44:57 +0200

    no message

---
+++ NetworkPrograming/Serialization (XML, JSON)/classFrog/Frog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace classFrog
+{
+    class Frog
+    {
+        public int age;
+        public string name;
+        public int[] par;
+
+
+    }
+    class Frog2
+    {
+        public int age;
+        public int[] par;
+        [JsonProperty("name")]
+        public string frogname;
+        
+
+
+    }
+}
---
+++ NetworkPrograming/Serialization (XML, JSON)/classFrog/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace classFrog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Frog frog = new Frog();
+            frog.name = "dds";
+            frog.age = 42;
+            frog.par = new int[] { 4, 5, 6 };
+
+            string output = JsonConvert.SerializeObject(frog);
+            Frog2 deserializedfrog = JsonConvert.DeserializeObject<Frog2>(output);
+
+            Console.WriteLine(output);
+            
+            Console.WriteLine(deserializedfrog);
+            Console.WriteLine(deserializedfrog.frogname);
+            Console.WriteLine(deserializedfrog.age);
+            Console.WriteLine(deserializedfrog.par[1]);
+          
+        }
+    }
+}
