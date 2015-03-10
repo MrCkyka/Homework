c2d27a0488fd1ebc66ff829f9e77fdced1df3914
mrckyka <namepodlec@yandex.ru>
2015-03-10 19:00:01 +0200

    no message

---
+++ WCF/6. WCF Service mex/WCF Service mex/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCF_Service_mex
+{
+    [ServiceContract]
+    public interface IMyMath
+    {
+        [OperationContract]
+        int Add(int a, int b);
+    }
+
+    public class MyMath : IMyMath
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(MyMath));
+            sh.Open();
+            Console.WriteLine("Для завершения нажмите <ENTER>\n");
+            Console.ReadLine();
+            sh.Close();
+        }
+    }
+}
