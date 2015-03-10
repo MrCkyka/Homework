5a3a0de6f7e529a463862cd1df9a0eacfb66eba7
mrckyka <namepodlec@yandex.ru>
2015-03-10 19:04:01 +0200

    no message

--- WCF/5. WCF Service mex/WcfService1/IService1.cs
+++ WCF/5. WCF Service mex/WcfService1/IService1.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace WcfService1
 {
-    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+   
     [ServiceContract]
     public interface IService1
     {
@@ -19,11 +19,11 @@ namespace WcfService1
         [OperationContract]
         CompositeType GetDataUsingDataContract(CompositeType composite);
 
-        // TODO: Добавьте здесь операции служб
+        
     }
 
 
-    // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+  
     [DataContract]
     public class CompositeType
     {
